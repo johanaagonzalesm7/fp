3fd95274f3d466ca264a9f1b99f1f7b3db3bdf62
johanaagonzalezm7 <johanaagonzalezm7@gmail.com>
2021-11-24 17:22:54 -0600

    Numeros Fibonacci

---
+++ Unidad 4/JAGM_TC4.1E1.py
@@ -0,0 +1,16 @@
+funtion [bandera] = esFibonacci(numero)
+z = int(input('Ingresa un numero'))
+pre == 0
+pos == 1
+bandera == false
+while (pre <= pos):
+	if pre == numero:
+		bandera == true
+	and
+and
+numero = input("Por favor ingresar un numero")
+if (esFibonacci(numero))
+	print ( "el numero si pertenece a la serie fibonacci")
+else
+	print ("el numero no pertenece a la serie fibonacci")
+and
