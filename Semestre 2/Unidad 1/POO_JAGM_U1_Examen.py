8f6b68e2547e9d4514b2ae1b6b61d0833649e7e2
johanaagonzalezm7 <johanaagonzalezm7@gmail.com>
2022-02-23 20:43:59 -0600

    Examen Unidad 1

--- Semestre 2/Unidad 1/POO_JAGM_U1_Examen.py
+++ Semestre 2/Unidad 1/POO_JAGM_U1_Examen.py
@@ -3,20 +3,28 @@ Contraseña="Johanam"
 U=str(input("Ingresa tu Usuario"))
 C=str(input("Ingresa tu contraseña"))
 i=1
-for i in range(4):
+
+while i>=4:
 	if C==Contraseña:
 		print("Bienvenido al sistema")
 	else:
 		print ("Contraseña o Usuario Incorrecto")
-print("Espera 2 minutos para intentar de nuevo")
-import time
-def countdown(t):
-	while t:
-		mins,secs=(t,60)
-		timer='{:02d}:{:02d}'.format
-		print(timer,end="\r")
-		time.sleep(1)
-		t-=1
-	print("El tiempo a termiando")
-t=5
-countdown(int(t))
+		print("Espera 2 minutos para intentar de nuevo")
+		import time
+		def countdown(t):
+			while t:
+				mins,secs=(t,60)
+				timer='{:02d}:{:02d}'.format
+				print(timer,end="\r")
+				time.sleep(1)
+				t-=1
+			print("El tiempo a termiando")
+		t=2
+		countdown(int(t))
+while i>=3:
+	if C==Contraseña:
+		print("Bienvenido al sistema")
+	else:
+		print ("Contraseña o Usuario Incorrecto")
+		print("Sistems Bloqueado")
+		break
--- Semestre 2/Unidad 1/POO_JAGM_U1_Examen1.py
+++ Semestre 2/Unidad 1/POO_JAGM_U1_Examen1.py
@@ -2,11 +2,39 @@ Usuario="Johana Macias"
 Contraseña="Johanam"
 U=str(input("Ingresa tu Usuario"))
 C=str(input("Ingresa tu contraseña"))
-i=0
-while i==4:
+i=120
+if C==Contraseña:
+	print("Bienvenido al sistema")
+else:
+	print("Contraseña o Usuario Incorrecto, Intenta de nuevo")
 	if C==Contraseña:
 		print("Bienvenido al sistema")
 	else:
 		print("Contraseña o Usuario Incorrecto, Intenta de nuevo")
-for i in range(1,4):
-	print("Intentos fallidos")
+		if C==Contraseña:
+			print("Bienvenido al sistema")
+		else:
+			print("Contraseña o Usuario Incorrecto, Intenta de nuevo")
+			if C==Contraseña:
+				print("Bienvenido al sistema")
+			else:
+				print("Contraseña o Usuario Incorrecto, Intenta de nuevo")
+				import time 
+					while i>0:
+						print(i)
+						i-=1
+						time.sleep(1)
+				    print("El tiempo ha termiando, Intenta de Nuevo")
+					if C==Contraseña:
+						print("Bienvenido al sistema")
+					else:
+						print("Contraseña o Usuario Incorrecto, Intenta de nuevo")
+						if C==Contraseña:
+							print("Bienvenido al sistema")
+						else:
+							print("Contraseña o Usuario Incorrecto, Intenta de nuevo")
+							if C==Contraseña:
+								print("Bienvenido al sistema")
+							else:
+								print("Contraseña o Usuario Incorrecto, Intenta de nuevo")
+								print("Intenta mas tarde")
